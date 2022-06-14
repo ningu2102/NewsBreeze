e4b1152ede1cbf44f2951206bdf36a27701bb8a0
Ninganna Kuntoji <ninganna2102@gmail.com>
2022-06-14 19:09:52 +0530

    detail view updated

--- app/src/main/java/com/nrk/newsbreeze/view/NewsDetailActivity.kt
+++ app/src/main/java/com/nrk/newsbreeze/view/NewsDetailActivity.kt
@@ -5,9 +5,11 @@ import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import com.nrk.newsbreeze.data.model.Article
 import com.nrk.newsbreeze.databinding.ActivityNewsDetailBinding
+import com.nrk.newsbreeze.utils.DateUtil
 import com.nrk.newsbreeze.viewmodel.NewsDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,25 +22,29 @@ class NewsDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar!!.hide()
+        getDataFromIntent()
+        setUi()
+    }
+
+    private fun getDataFromIntent() {
         val gson = Gson()
         article = gson.fromJson(intent.getStringExtra("selectedArticle"), Article::class.java)
-        setUi()
     }
 
     private fun setUi() {
         binding.apply {
-            val article = article
-            webView.apply {
-                webViewClient = WebViewClient()
-                article.url?.let {
-                    loadUrl(article.url.toString())
-                }
-            }
+            Glide.with(this@NewsDetailActivity)
+                .load(article.urlToImage)
+                .into(ivArticleImage)
+            tvDate.text = DateUtil.changeDateFormat(article.publishedAt)
+            tvTitle.text = article.title
+            tvAuthor.text = article.author
+            tvAuthorDesc.text = article.source!!.name
+            tvDesc.text = article.description
 
-            fab.setOnClickListener {
-                if (article != null) {
-                    viewModel.saveArticle(article)
-                }
+            btnSave.setOnClickListener {
+                viewModel.saveArticle(article)
                 Toast.makeText(
                     this@NewsDetailActivity,
                     "Note saved successfully",
